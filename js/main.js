17f0674871c3406f8319477528619fef0cf7086e
silfiananurhamida <silfianahamidah@gmail.com>
2019-12-02 10:22:52 +0700

    first landing oage

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ jQuery(document).ready(function($) {
 
 	"use strict";
 
-	$(".loader").delay(1000).fadeOut("slow");
-  $("#overlayer").delay(1000).fadeOut("slow");	
+	// $(".loader").delay(1000).fadeOut("slow");
+ //  $("#overlayer").delay(1000).fadeOut("slow");	
 
 	var siteMenuClone = function() {
 
